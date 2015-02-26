93020a7bb54dd5543271e0b5f8aaced331b7a17d
Deadhead84 <Jon@Scoville-MacBook-2.local>
2015-02-25 19:40:12 -0700

    updated app code

---
+++ app/time/time.js
@@ -0,0 +1,13 @@
+var app = angular.module('leApp');
+
+app.directive('theTime', function(){ // the-time in html is theTime in javascript directive (it autoplaces a hiphen in it)
+	return {
+		restrict: 'E',
+		template: 'The Time is: {{time}}',// {{time | date: "medium"}} with Date.now() (this is the angular specific syntax)
+		link: function($scope, element, attrs) {
+			var currentTime = new Date(); //Date.now()
+			$scope.time = currentTime.toString();
+		}
+	}
+
+})
